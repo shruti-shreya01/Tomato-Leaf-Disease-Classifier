a5526cdcd3486936d412b7f4e5f37b9ab7216d35
Shruti Shreya <shreyaashruti01@gmail.com>
2024-10-06 15:19:45 +0530

    Update potato_streamlit.py

--- potato_streamlit.py
+++ potato_streamlit.py
@@ -127,7 +127,7 @@ def load_model_from_pickle(pickle_path):
     model = tf.keras.models.model_from_json(data["architecture"])
     
     # Load the weights from the file
-    model.load_weights(data["model_weights"])
+    model.load_weights(data["weights"])
 
     # Compile the model if needed
     model.compile(
